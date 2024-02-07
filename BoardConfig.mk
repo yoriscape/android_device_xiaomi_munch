497bda16b95a8e3427549cf064a53a3302afd067
yoriscape <sukhpreetsingh58627@gmail.com>
2024-02-07 06:01:09 +0100

    munch: Switch to VULKAN UI renderer

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,7 @@ TARGET_INIT_VENDOR_LIB ?= //$(DEVICE_PATH):libinit_munch
 TARGET_RECOVERY_DEVICE_MODULES ?= libinit_munch
 
 # HWUI
+TARGET_USES_VULKAN := true
 HWUI_COMPILE_FOR_PERF := true
 
 # Kernel
