3c6f2672e5cebd6c6166a8331f4acb34b9d2a806
EmanuelCN <emanuelghub@gmail.com>
2024-06-16 17:05:13 +0000

    munch: Override cgroups.json & task_profiles.json * In order to allow uclamp kernels to place tasks correctly

--- device.mk
+++ device.mk
@@ -178,6 +178,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
