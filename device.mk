b00ee19975b98bc1f7db9e27abef8e3812c57a8a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-06-16 10:28:54 +0000

    munch: Import XiaomiParts from sdm8250-common
    Signed-off-by: yoriscape <sukhpreetsingh58627@gmail.com>

--- device.mk
+++ device.mk
@@ -187,6 +187,9 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
